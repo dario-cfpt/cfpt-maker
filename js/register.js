a4112596482554a36a2bc43956b629fa804d0f5f
dario-cfpt <dario.gng@eduge.ch>
2019-11-12 15:39:47 +0100

    Fix js folder name and simplify the register js

---
+++ js/register.js
@@ -0,0 +1,34 @@
+function checkPassword(str) {
+    // at least one number, one lowercase and one uppercase letter
+    // at least six characters
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}/;
+    return re.test(str);
+}
+
+function sendPost(user) {
+    $.ajax({
+        type: "POST",
+        url: 'http://127.0.0.1:3000/user',
+        data: user,
+        success: function(response) {
+            console.log(response);
+            // TODO: manage response
+        },
+        error: function(e) {
+            console.log(e.responseText);
+        }
+    });
+}
+
+$(function() {
+    $('form').submit(function(e) {
+        if ($("input#password").val() == $("input#password-check").val()) {
+            const user = {};
+            user.email = $("input#mail").val().trim();
+            user.username = $("input#username").val().trim();
+            user.password = $("input#password").val();
+            sendPost(user);
+        }
+        e.preventDefault();
+    });
+});
