ac8c62a95de317ff5d8756d98c9a2dece43bba88
Tom <rysertom@gmail.com>
2019-11-21 21:50:29 +0100

    style correction

--- website/profile.php
+++ website/profile.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="style.css" rel="stylesheet" type="text/css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous">
@@ -20,15 +21,9 @@
     ?>
     <div class="row" style="padding: 44px">
         <div class="col-lg-8">
-            <div id="madeBy">
-
-            </div>
-            <div id="score">
-
-            </div>
-            <div id="played">
-
-            </div>
+            <div class="row" id="madeBy"></div>
+            <div class="row" id="score"></div>
+            <div class="row" id="played"></div>
         </div>
         <div class="col">
             <p id="userName"></p>
